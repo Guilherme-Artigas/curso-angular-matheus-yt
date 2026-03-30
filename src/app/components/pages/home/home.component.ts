0d9bba7e84cd1fa967533fbba7941c62af076d3c
Guilherme Artigas <guilherme.artigas92@gmail.com>
2026-03-30 15:53:18 -0300

    feat: implement home component; add service for fetching moments and display list in template

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Moment } from 'src/app/interfaces/Moment';
+import { HomeService } from './home.service';
+import { MessageService } from '../../message/message.service';
+import { Response } from 'src/app/interfaces/Response';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public allMoments: Moment[] = [];
+  public momentsFiltered: Moment[] = [];
 
-  constructor() { }
+  constructor(
+    private readonly homeService: HomeService,
+    private readonly messageService: MessageService
+  ) { }
 
   ngOnInit(): void {
+    this.homeService.getAll().subscribe({
+      next: (response: Response<Moment[]>) => {
+        this.allMoments = response.data;
+      },
+      error: (err) => {
+        this.messageService.showMessage('Erro ao buscar momentos.');
+        console.error('Erro ao buscar momentos: ', err);
+      }
+    });
   }
 
 }
