e678374b689320eee9a15bb7e7bf61734f31a117
Guilherme Artigas <guilherme.artigas92@gmail.com>
2026-03-31 14:21:58 -0300

    feat: implement comment functionality; add form for comments with validation and service integration

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -5,6 +5,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { MessageService } from '../../message/message.service';
 import { environment } from 'src/environments/environment';
 import { faEdit, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FormControl, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
+import { Comment } from 'src/app/interfaces/Comment';
 
 @Component({
   selector: 'app-moment',
@@ -19,6 +21,8 @@ export class MomentComponent implements OnInit {
   public faTimes = faTimes;
   public faEdit = faEdit;
 
+  public commentForm!: FormGroup;
+
   constructor(
     private readonly momentService: MomentService,
     private readonly activatedRoute: ActivatedRoute,
@@ -28,6 +32,7 @@ export class MomentComponent implements OnInit {
 
   ngOnInit(): void {
     this.id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+
     this.momentService.getOne(this.id).subscribe({
       next: (response) => {
         this.moment = response.data;
@@ -35,6 +40,40 @@ export class MomentComponent implements OnInit {
       error: (err) => {
         this.messageService.showMessage('Houve erro ao buscar detalhes do momento.');
         console.error('Houve erro ao buscar detalhes do momento: ', err);
+      }
+    });
+
+    this.commentForm = new FormGroup({
+      text: new FormControl('', Validators.required),
+      username: new FormControl('', Validators.required),
+    });
+  }
+
+  public get text() {
+    return this.commentForm.get('text')!;
+  }
+
+  public get username() {
+    return this.commentForm.get('username')!;
+  }
+
+  public createComment(formDirective: FormGroupDirective) {
+    if (this.commentForm.invalid) return;
+
+    const data: Comment = this.commentForm.value;
+
+    data.momentId = Number(this.moment.id);
+
+    this.momentService.newComment(data).subscribe({
+      next: (response) => {
+        this.messageService.showMessage('Comentário foi adicionado com sucesso!');
+        this.commentForm.reset();
+        formDirective.resetForm();
+        this.moment.comments?.push(response.data);
+      },
+      error: (err) => {
+        this.messageService.showMessage('Erro ao adicionar comentário!');
+        console.error('Erro ao adicionar comentário: ', err);
       }
     });
   }
--- src/app/components/pages/moment/moment.service.ts
+++ src/app/components/pages/moment/moment.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Comment } from 'src/app/interfaces/Comment';
 import { Moment } from 'src/app/interfaces/Moment';
 import { Response } from 'src/app/interfaces/Response';
 import { environment } from 'src/environments/environment';
@@ -10,8 +11,14 @@ import { environment } from 'src/environments/environment';
 })
 export class MomentService {
   private readonly apiUrl: string = `${environment.apiUrl}/api/moments`;
+  private readonly apiUrlComment: string = `${environment.apiUrl}/api/moments`;
 
   constructor(private readonly httpClient: HttpClient) { }
+
+  public newComment(data: Comment): Observable<Response<Comment>> {
+    const url: string = `${this.apiUrlComment}/${data.momentId}/comments`;
+    return this.httpClient.post<Response<Comment>>(url, data);
+  }
 
   public getOne(id: number): Observable<Response<Moment>> {
     return this.httpClient.get<Response<Moment>>(`${this.apiUrl}/${id}`);
